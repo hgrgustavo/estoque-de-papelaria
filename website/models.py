cb27972f8a2d51fc1d7aa2860202d4ddbb86ceeb
gus <gustavohgrdev@gmaiil.com>
2025-02-28 17:16:42 -0300

    fix:

--- website/models.py
+++ website/models.py
@@ -1,49 +1,48 @@
-
 from django.db import models
-from django.core import validators
-from datetime import datetime
+
+class Livros(models.Model):
+    título = models.CharField(max_length=45)
+    isbn = models.CharField(max_length=17)
+    edição = models.CharField(max_length=45)
+    editora = models.CharField(max_length=45)
+    ano_publicação = models.TextField()  # This field type is a guess.
+    preço_capa = models.FloatField()
+    categoria = models.CharField(max_length=45)
+
+    class Meta:
+        
+        db_table = 'livros'
 
 
 class Autores(models.Model):
-    nome_completo = models.CharField(max_length=255)
+    nome_completo = models.CharField(max_length=45)
     nacionalidade = models.CharField(max_length=45)
-    biografia = models.CharField(max_length=1000)
+    biografia = models.TextField()
 
     class Meta:
         
-        db_table = 'cadastro_autores'
+        db_table = 'autores'
 
 
-class Livros(models.Model):
-    titulo = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=17, validators=[validators.RegexValidator(regex=r'^(?:\d[\ |-]?){13}$')])
-    edição = models.CharField(max_length=255)
-    editora = models.CharField(max_length=255)
-    ano_publicação = models.IntegerField(validators=[validators.MaxValueValidator(datetime.now().year + 2)])  # This field type is a guess.
-    preço_capa = models.FloatField()
-    categoria = models.CharField(max_length=255)
+class Estoque(models.Model):
+    data_entrada = models.DateField()
+    data_saida = models.DateField()
+    livros = models.ForeignKey('Livros', models.DO_NOTHING)
 
     class Meta:
         
-        db_table = 'cadastro_livros'
+        db_table = 'estoque'
+        unique_together = (('id', 'livros'),)
+
+
+
 
 
 class LivrosHasAutores(models.Model):
-    cadastro_livros = models.OneToOneField(Livros, models.DO_NOTHING, primary_key=True)  # The composite primary key (cadastro_livros_id, cadastro_autores_id) found, that is not supported. The first column is selected.
-    cadastro_autores = models.ForeignKey(Autores, models.DO_NOTHING)
+    livros = models.OneToOneField(Livros, models.DO_NOTHING, primary_key=True)  
+    autores = models.ForeignKey(Autores, models.DO_NOTHING)
 
     class Meta:
         
-        db_table = 'cadastro_livros_has_cadastro_autores'
-        unique_together = (('cadastro_livros', 'cadastro_autores'),)
-
-
-class ControleEstoque(models.Model):
-    cadastro_livros = models.ForeignKey(Livros, models.DO_NOTHING)
-    data_entrada = models.DateField()
-    data_saida = models.DateField()
-
-    class Meta:
-        
-        db_table = 'controle_estoque'
-        unique_together = (('id', 'cadastro_livros'),)
+        db_table = 'livros_has_autores'
+        unique_together = (('livros', 'autores'),)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from . import models
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from . import models
 
-
-class Index(TemplateView):
+class Indexpage(TemplateView):
     template_name = "index.html"
+
+
+class Cadastros(TemplateView):
+    template_name = "cadastros.html"
+
+class CadastroLivros(CreateView):
+    template_name = "cadastro_livros.html"
+    model = models.Livros
+
+class CadastroAutores(CreateView):
+    template_name = "cadastro_autores.html"
+    model = models.Livros
+
+class Estoque(CreateView):
+    template_name = "estoque.html"
+    model = models.Livros
+
+
+
